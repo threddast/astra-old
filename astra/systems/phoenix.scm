fb59dfb831c86dfd0c4b4330d1338614b9eb9197
Erik Giorgis <egiorgis@egiorg.is>
2023-03-10 21:47:50 +0100

    Added proper Phoenix uuids

--- astra/systems/phoenix.scm
+++ astra/systems/phoenix.scm
@@ -18,15 +18,15 @@
    (list
     (file-system
      (mount-point "/boot/efi")
-     (device (uuid "1ADC-28E8" 'fat32))
+     (device (uuid "124B-646C" 'fat32))
      (type "vfat"))
     (file-system
      (mount-point "/")
      (device
-      (uuid "6c3ee1c8-6ee6-4142-b2bf-a370854f63e7"
+      (uuid "d17ee161-a77e-456d-b2dc-3c07aa9f61f9"
             'ext4))
      (type "ext4")))
    #:swap-devices
    (list
     (swap-space
-     (target (uuid "62f47965-ad3e-40a9-bb5e-46e4387fa449")))))))
+     (target (uuid "9b242425-fd79-43a5-ad42-b217e9e3ea2c")))))))
