5f8ce02f567c7a7e6b84e685719a0babf43b5c60
Erik Giorgis <egiorgis@egiorg.is>
2023-03-11 10:27:50 +0100

    error correction

--- astra/systems.scm
+++ astra/systems.scm
@@ -49,7 +49,18 @@
                 (bootloader grub-efi-bootloader)
                 (targets '("/boot/efi"))))
    (services '())
-   (file-systems %base-file-systems)
+   (file-systems (cons* (file-system
+                          (mount-point "/boot/efi")
+                          (device (uuid "124B-646C"
+                                        'fat32))
+                          (type "vfat"))
+                        (file-system
+                          (mount-point "/")
+                          (device (uuid
+                                   "d17ee161-a77e-456d-b2dc-3c07aa9f61f9"
+                                   'ext4))
+                          (type "ext4"))
+                        %base-file-systems))
    (sudoers-file #f)
    (issue "This is the GNU/Engstrand system. Welcome.\n")))
 
